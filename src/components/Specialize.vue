bce65f5b7b8c30444207053e1b8f0c8bd69ea6cd
Resume Website <jdcollins242@gmail.com>
2020-11-06 08:06:31 -0500

    Update Specialize sectioon

--- src/components/Specialize.vue
+++ src/components/Specialize.vue
@@ -5,34 +5,44 @@
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
             <div class="bottom-line"></div>
             <p class="lead">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Unde reprehenderit eos dolorem beatae.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde
+                reprehenderit eos dolorem beatae.
             </p>
             <div class="specials container">
-                <div>
+                <!-- <div>
                     <i class="fa fa-robot"></i>
                     <h3>Machine Learning</h3>Lorem ipsum, dolor sit amet consectetur adipisicing
                     elit. Esse tempore ipsa iusto? Amet, delectus nesciunt.
-                </div>
+                </div> -->
                 <div>
                     <i class="fab fa-chrome"></i>
-                    <h3>Fullstack Web Development</h3>Lorem ipsum, dolor sit amet consectetur adipisicing
-                    elit. Esse tempore ipsa iusto? Amet, delectus nesciunt.
+                    <h3>Fullstack Web Development</h3>
+                    <p class="specialize-description">
+                        I am a Fullstack Developer. The tools I work with are
+                        mainly Vue, React, Node, and MySQL. However, I am always
+                        open to learning new technologies. I ensure that
+                        applications are beautiful, performant, and mobile
+                        responsive.
+                    </p>
                 </div>
-                <div>
+                <!-- <div>
                     <i class="fas fa-mobile"></i>
                     <h3>Mobile App Development</h3>Lorem ipsum, dolor sit amet consectetur adipisicing
                     elit. Esse tempore ipsa iusto? Amet, delectus nesciunt.
-                </div>
-                <div>
+                </div> -->
+                <!-- <div>
                     <i class="fas fa-gamepad"></i>
                     <h3>Game Development</h3>Lorem ipsum, dolor sit amet consectetur adipisicing
                     elit. Esse tempore ipsa iusto? Amet, delectus nesciunt.
-                </div>
+                </div> -->
                 <div>
                     <i class="fas fa-brush"></i>
-                    <h3>Graphic Design</h3>Lorem ipsum, dolor sit amet consectetur adipisicing
-                    elit. Esse tempore ipsa iusto? Amet, delectus nesciunt.
+                    <h3>Graphic Design</h3>
+                    <p class="specialize-description">
+                        I have an extensive background in graphic design. I
+                        specialize in logos, flyers, business
+                        cards, and mobile and desktop website mockups.
+                    </p>
                 </div>
             </div>
         </div>
@@ -43,13 +53,14 @@
 export default {};
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
 @import url("https://fonts.googleapis.com/css2?family=Dosis:wght@400;600&display=swap");
 
 #specialize {
     background-color: #f3f3f3;
     text-align: center;
     font-family: "Dosis";
+    margin: 0 auto;
 }
 
 h1 {
@@ -60,18 +71,24 @@ h1 {
 
 .specials {
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(2, 1fr);
     margin: 2rem 0;
 }
 
 .specials div {
     margin: 2rem;
+
+    .specialize-description {
+        padding-top: 0.5rem;
+        width: 60%;
+        margin: auto;
+    }
 }
 
 .container {
     max-width: 1280px;
     padding: 4rem;
-    margin:  auto;
+    margin: auto;
     overflow: hidden;
 }
 
